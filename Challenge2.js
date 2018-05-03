830860bf7e016c5b507eaa4ba8941b60d560b4aa
celestebancos <celestebancos@gmail.com>
2018-05-03 12:48:54 -0400

    Add go function

--- Challenge2.js
+++ Challenge2.js
@@ -1,36 +1,44 @@
 {
 	init: function(elevators, floors) {
-		var elevator = elevators[0];
-		// var floorsWanted = [];
-		// for (index = 0; index <= floors.length(); index++){
-		// 	floorsWanted.push(false);
-		// 	console.log(index);
-		// }
-		// console.log(floorsWanted);
+		var elevator = elevators[0]; // Let's use the first elevator
+		var floorsWanted = [];
+		for (var index = 0; index < floors.length; index++){
+			floorsWanted.push(false);
+		};
 
+		// Whenever the elevator is idle (has no more queued destinations) ...
 		elevator.on("idle", function() {
-			for (i = 1; i<= 4; i++){
-				console.log("Floor " + i + "pressed = " + isButtonPressed(elevator, i));
+			for (var i = 1; i<= 4; i++){
+				// console.log("Floor " + i + "pressed = " + isButtonPressed(elevator, i));
 				if (isButtonPressed(elevator, i)){
-					elevator.goToFloor(i);
+					go(elevator, i);
 				}
 			}
 			elevator.goToFloor(0);
 		});
 
-		function isButtonPressed (elevator, floor){
+		//checks if an elevator button is pressed
+		function isButtonPressed(elevator, floor){
 			if (elevator.getPressedFloors().includes(floor)){
 				return true;
 			} else {
 				return false;
 			}
+		};
+
+		//goes to a floor and turns off the wanted status
+		function go(elevator, floor){
+			elevator.goToFloor(floor);
+			floorsWanted[floor] = false;
+			console.log(floorsWanted);
 		}
 	},
+	update: function(dt, elevators, floors) {
+		// We normally don't need to do anything here
 
-		update: function(dt, elevators, floors) {
-		}
+
+		//Okay, so my idea is to have a floors wanted array, that stores boolean values for all the floors.
+		//I'll make a new elevator.go() function that goes to the floor and sets the value to false for that floor.
+		//And when a button is pressed, I'll set the value to true.
+	}
 }
-
-//Okay, so my idea is to have a floors wanted array, that stores boolean values for all the floors.
-//I'll make a new elevator.go() function that goes to the floor and sets the value to false for that floor.
-//And when a button is pressed, I'll set the value to true.
