942bff06f5c519c951e63b293ac00abf882fb818
celestebancos <celestebancos@gmail.com>
2018-05-03 21:23:21 -0400

    Add another elevator

--- Challenge2.js
+++ Challenge2.js
@@ -1,6 +1,5 @@
 {
 	init: function(elevators, floors) {
-		var elevator = elevators[0]; // Let's use the first elevator
 		var floorsWanted = [];
 		for (var index = 0; index < floors.length; index++){
 			floorsWanted.push(false);
@@ -17,15 +16,21 @@
 			});
 		}
 
-		// Whenever the elevator is idle (has no more queued destinations) ...
-		elevator.on("idle", function() {
-			for (var i = 1; i<= 4; i++){
-				if (isFloorWanted(elevator, i)){
-					go(elevator, i);
-				}
-			}
-			elevator.goToFloor(0);
+		// adds idle event handler to elevators
+		elevators.forEach(function(element){
+			idle(element);
 		});
+
+		function idle(elevator){
+			elevator.on("idle", function(){
+				for (var i = 1; i<= floors.length; i++){
+					if (isFloorWanted(elevator, i)){
+						go(elevator, i);
+					}
+				}
+				elevator.goToFloor(0);
+			});
+		}
 
 		//checks if an elevator or floor button is pressed
 		function isFloorWanted(elevator, floor){
@@ -45,10 +50,5 @@
 	},
 	update: function(dt, elevators, floors) {
 		// We normally don't need to do anything here
-
-
-		//Okay, so my idea is to have a floors wanted array, that stores boolean values for all the floors.
-		//I'll make a new elevator.go() function that goes to the floor and sets the value to false for that floor.
-		//And when a button is pressed, I'll set the value to true.
 	}
 }
